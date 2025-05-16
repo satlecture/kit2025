92244f2b394caf97c6cd640a4f77d8fd523effa6
Dominik Schreiber <mail@dominikschreiber.de>
2025-05-16 16:40:17 +0200

    Benchmarks for SDVSTP

--- code/src/sdvstp.cc
+++ code/src/sdvstp.cc
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <iostream>
 #include <fstream>
+#include <random>
 #include <sstream>
 #include <utility>
 #include <vector>
@@ -21,6 +22,7 @@ const Pattern P_COPPER  {1, 3};
 const Pattern P_IRON    {1, 5};
 const Pattern P_GOLD    {3, 3};
 const Pattern P_IRIDIUM {3, 6};
+const std::vector<Pattern> PATTERNS = {P_BASE, P_COPPER, P_IRON, P_GOLD, P_IRIDIUM};
 
 // Template for a single action. "p" should be one of the five patterns above,
 // then "rotate" indicates whether the pattern should still be rotated.
@@ -32,7 +34,7 @@ struct Grid {
 private:
     std::vector<std::vector<CellState>> states;
 public:
-    Grid(int w, int h) : states(h, std::vector<CellState>(w)) {}
+    Grid(int w, int h) : states(h, std::vector<CellState>(w, TO_BE_TILLED)) {}
     int height() const {return states.size();}
     int width() const {assert(states.size() > 0); return states[0].size();}
     CellState at(int x, int y) const {return states[y][x];}
@@ -42,27 +44,81 @@ public:
         return a.x + a.p.width <= width()
             && a.y + a.p.height <= height();
     }
-    void apply(Action a) {
+    void apply(Action a, CellState targetState = TILLED) {
         if (!applicable(a)) throw std::runtime_error("Action not applicable!");
         if (a.rotate) a.p.rotate(); // rotate pattern if necessary
         // Apply pattern to the grid
         for (int offsetX = 0; offsetX < a.p.width; offsetX++) {
             for (int offsetY = 0; offsetY < a.p.height; offsetY++) {
-                at(a.x + offsetX, a.y + offsetY) = TILLED;
+                at(a.x + offsetX, a.y + offsetY) = targetState;
             }
         }
     }
-    void print() const {
-        std::cout << "c Grid (" << width() << " x " << height() << "):" << std::endl;
+    void print(std::ostream* osPtr = &std::cout, bool comment = true) const {
+        auto& os = *osPtr;
+        if (comment) os << "c Grid (" << width() << " x " << height() << "):" << std::endl;
         for (int y = 0; y < height(); y++) {
-            std::cout << "c ";
+            if (comment) os << "c ";
             for (int x = 0; x < width(); x++) {
-                std::cout << std::string(1, at(x, y));
+                os << std::string(1, at(x, y));
             }
-            std::cout << std::endl;
+            os << std::endl;
         }
+    }
+    // Just for generating
+    float tillableRatio() const {
+        int tillable = 0;
+        for (int y = 0; y < height(); y++) {
+            for (int x = 0; x < width(); x++) {
+                tillable += at(x, y) == TO_BE_TILLED;
+            }
+        }
+        return tillable / (float) (width() * height());
     }
 };
+
+// Generate a random grid. Used for benchmark generation.
+Grid generateRandomGrid(int width, int height, float targetTillableRatio = 0.7, float minTillableRatio = 0.5) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distCoin(0, 1);
+    std::uniform_int_distribution<int> distPosX(0, width-1);
+    std::uniform_int_distribution<int> distPosY(0, height-1);
+    std::uniform_int_distribution<size_t> distSmallPattern(0, 1);
+
+    Grid grid(width, height);
+    int itCount = 0;
+    while (itCount < 10'000 && grid.tillableRatio() > targetTillableRatio) {
+        itCount++;
+        // Select a random small pattern
+        Pattern p = PATTERNS[distSmallPattern(gen)];
+        Action a {p, distCoin(gen) > 0, distPosX(gen), distPosY(gen)};
+        if (!grid.applicable(a)) continue;
+        Grid copy = grid;
+        copy.apply(a, distCoin(gen) ? TILLED : NOT_TO_BE_TILLED);
+        if (copy.tillableRatio() < minTillableRatio) continue;
+        grid = copy;
+    }
+    return grid;
+}
+// Generate a set of benchmarks to the specified output path.
+// Not necessarily the (only) set of benchmarks we evaluate the solvers with.
+void generateBenchmarks(const std::string& outPath) {
+    for (int width = 3; width <= 32; width = std::max((int) (1.2*width), width+1)) {
+        for (int height = 3; height <= width; height = std::max((int) (1.3*height), height+1)) {
+            for (int rep = 1; rep <= 5; rep++) {
+                Grid grid = generateRandomGrid(width, height);
+                auto outFile = outPath + "/gen-w" + std::to_string(width) + "-h"
+                    + std::to_string(height) + "-rep" + std::to_string(rep) + ".sdvstp";
+                std::ofstream ofs(outFile);
+                ofs << "p sdvstp " << width << " " << height << std::endl;
+                grid.print(&ofs, false);
+                ofs.close();
+                std::cout << "(" << width << ", " << height << ", " << rep << ") : written to " << outFile << std::endl;
+            }            
+        }   
+    }
+}
 
 // Parse a .sdvstp problem file and return the corresponding Grid instance.
 Grid parseSDVSTPFile(const std::string& filename) {
@@ -108,6 +164,11 @@ Grid parseSDVSTPFile(const std::string& filename) {
 }
 
 int main(int argc, char* argv[]) {
+
+    // For benchmark generation:
+    //generateBenchmarks("../exercises/sdvstp/benchmarks/");
+    //exit(0);
+
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
         return 1;
